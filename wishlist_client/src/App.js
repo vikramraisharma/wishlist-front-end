4406cc417e6e5bd7ba2be6acd9f883fa0df58342
Ric Mershon <rmersh@ricmershon.com>
2020-04-03 15:23:37 -0400

    First pass.

--- wishlist_client/src/App.js
+++ wishlist_client/src/App.js
@@ -1,13 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// =============================
+// DEPENDENCIES
+// =============================
+// packages
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App test</h1>
-    </div>
-  );
+// components
+import Header from './components/Header.js'
+import Aside from './components/Aside.js'
+import Main from './components/Main.js'
+
+// =============================
+// COMPONENT CLASS
+// =============================
+class App extends React.Component {
+  state = {
+    view: {
+      page: 'home',
+      pageTitle: 'I heard of that...'
+    },
+    formInputs: {
+      name: null,
+      image: null,
+      body: null,
+      id: null
+    }
+  }
+
+  handleView = (view, post) => {
+    let pageTitle = ''
+    let formInputs = {
+      name: '',
+      image: '',
+      body: '',
+      id: null
+    }
+
+    switch(view) {
+      case 'home':
+        pageTitle = 'I heard that...'
+        break
+      case 'addPost':
+        pageTitle = 'What did you say?'
+        break
+      case 'editPost':
+        pageTitle = 'What did you really say?'
+        formInputs = {
+          name: post.name,
+          image: post.iamge,
+          body: post.body,
+          id: post.id
+        }
+        break
+      default:
+        break
+    }
+
+    this.setState ({
+      view: {
+        page: view,
+        pageTitle: pageTitle
+      },
+      formInputs: formInputs
+    })
+  }
+
+  // ==============
+  // RENDER
+  // ==============
+  render () {
+    return (
+      <div className="large-container">
+        <Header/>
+        <div className="main-container">
+          <Main
+            view={this.state.view}
+            formInputs={this.state.formInputs}
+            handleView={this.handleView}/>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+// =============================
+// EXPORT
+// =============================
+export default App
---
+++ wishlist_client/src/components/Wishlist.js
@@ -0,0 +1,43 @@
+// =============================
+// DEPENDENCIES
+// =============================
+// packages
+import React from 'react'
+
+// =============================
+// COMPONENT CLASS
+// =============================
+class Post extends React.Component {
+  // ==============
+  // RENDER
+  // ==============
+  render () {
+    return (
+      <article>
+        <div className="post-header">
+          <img src={this.props.post.image} alt="img"/>
+          <h1>{this.props.name} said...</h1>
+        </div>
+        <div className="post-body">
+          {this.props.post.body}
+        </div>
+        <div className="post-options">
+          <ul>
+            <li onClick={() => {
+              this.props.handleView('editPost',
+              this.props.post)
+            }}>edit post</li>
+            <li onClick={() => {
+              this.props.handleDelete(this.props.post.id)
+            }}>delete post</li>
+          </ul>
+        </div>
+      </article>
+    )
+  }
+}
+
+// =============================
+// EXPORT
+// =============================
+export default Post
